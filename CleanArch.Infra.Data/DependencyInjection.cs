abcc89d6cd8967ae52788b7eedb7be34ecd36402
Abimael Mendes <abimaelmends@hotmail.com>
2023-09-23 03:40:24 -0300

    Add project files.

---
+++ CleanArch.Infra.Data/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.Infra.Data.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration  Configuration)
+        {
+                services.AddEntityFrameworkSqlServer()
+               .AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+                return services;
+        }
+        
+    }
+}
